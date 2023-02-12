0585e3e2d197bb7c23f5b01112bc78bf5fffae1c
Artyom <77617130+PlayinPistols2d@users.noreply.github.com>
2023-02-12 17:30:36 +0300

    Update mainwindow.cpp
    Algorithm revamped with some bug fixes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,9 +12,13 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    QString json = "{\"error\":{\"errorcode\":1,\"errorstring\":\"\",\"smth\":{\"value\":{\"chips\":[\"qxc\",\"sys\",3]}}},\"login\":\"qwerty\",\"logintoken\":{\"token\":\"4daaf6b3dd5a26a2ad2c436e564bfa4d6c439ce4d0d6cd66705a8bdadddddaa0\"}}";
+    QString json1 = "{\"error\":{\"errorcode\":1,\"errorstring\":\"\",\"smth\":{\"value\":{\"chips\":[\"qxc\",\"sys\",3]}}},\"login\":\"qwerty\",\"logintoken\":{\"token\":\"4daaf6b3dd5a26a2ad2c436e564bfa4d6c439ce4d0d6cd66705a8bdadddddaa0\"}}";
+    QString json2 = "{\"condition\":\"errorcode\",\"else\":{\"TYPE\":\"CO\",\"data\":{\"Text\":\"qwert\",\"Type\":0,\"num\":2}},\"then\":{\"TYPE\":\"CO1\",\"data\":{\"Text\":\"qqcxq\",\"Type\":1,\"num\":3}}}";
+    QString json = "{\"condition\":\"errorcode\",\"else\":{\"data\":{\"num\":2,\"Text\":\"qwert\",\"Type\":0},\"TYPE\":\"CO\"},\"then\":{\"data\":{\"num\":3,\"Text\":\"qqcxq\",\"Type\":1},\"TYPE\":\"CO1\"}}";
 
     temp = QJsonDocument::fromJson(json.toUtf8());
+
+    qDebug() << temp;
     QJsonObject obj  = temp.object();
 
     QStandardItemModel *model = new QStandardItemModel();
@@ -34,34 +38,30 @@ void MainWindow::parseJsonToTreeView(QJsonObject convertedJsonString, QStandardI
 {
     QJsonObject::iterator it;
     qDebug() << "Json string: " << convertedJsonString << endl;
-    isTopLevel = false;
     for(it=convertedJsonString.begin();it!=convertedJsonString.end();it++) {
+        QStandardItem *item;
         if(it.value().isObject()) {
             qDebug() << " Object : " << it.key() << " Value : "<< it.value() << "Proceed calculations..." << endl;
 
-            QStandardItem *item = new QStandardItem(it.key());
-            if((parent == NULL) || isTopLevel) {
+            item = new QStandardItem(it.key());
+            if(parent == NULL) {
                 model->appendRow(item);
             }
             else {
                 parent->appendRow(item);
             }
-            parent = item;
             qDebug() << " ... sending parent: " << parent << "made from item: " << item << endl;
-            parseJsonToTreeView(it.value().toObject(), model, parent);
+            parseJsonToTreeView(it.value().toObject(), model, item);
         }
         else {
             qDebug() << " Finished Object: " << it.key() << "Value : "<< it.value() << " Parent: " << parent << endl;
 
-            QStandardItem *item = new QStandardItem(it.key() + " - " + it.value().toString());
+            item = new QStandardItem(it.key() + " - " + it.value().toString());
             if (it.value().isDouble()){
                 item = new QStandardItem(QString::number(it.value().toInt()));
-                qDebug() << "Int" << endl;
             } else if (it.value().isBool()) {
                 item = new QStandardItem(it.value().toBool());
-                qDebug() << "Bool" << endl;
             } else if (it.value().isArray()) {
-                qDebug() << "Array" << endl;
                 QJsonArray array = it.value().toArray();
                 QJsonArray::iterator arrayIterator;
                 QString convertedToStringArray;
@@ -76,13 +76,10 @@ void MainWindow::parseJsonToTreeView(QJsonObject convertedJsonString, QStandardI
                 convertedToStringArray.chop(2);
                 item = new QStandardItem(convertedToStringArray);
             }
-            if(isTopLevel) {
+            if(parent == NULL) {
                 model->appendRow(item);
             }else {
                 parent->appendRow(item);
-            }
-            if(convertedJsonString.length()==1) {
-                isTopLevel = true;
             }
         }
     }
